02d8506a15703a1583af027eddd3693d77c15e7c
clara <cguzzetti@itba.edu.ar>
2020-10-28 19:53:17 -0300

    Fix sort query3 and check neighborhood condition for query2

--- api/src/main/java/ar/edu/itba/pod/g3/api/query2/Query2Mapper.java
+++ api/src/main/java/ar/edu/itba/pod/g3/api/query2/Query2Mapper.java
@@ -2,15 +2,28 @@ package ar.edu.itba.pod.g3.api.query2;
 
 import ar.edu.itba.pod.g3.api.models.TreeData;
 import ar.edu.itba.pod.g3.api.models.Tuple;
+import com.hazelcast.core.IMap;
 import com.hazelcast.mapreduce.Context;
 import com.hazelcast.mapreduce.Mapper;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Query2Mapper implements Mapper<String, TreeData, Tuple<String, String>, Integer> {
 
     private static final Integer ONE = 1;
+    private final Set<String> neighbourhoods;
+
+    public Query2Mapper(Set<String> neighbourhoods) {
+        this.neighbourhoods = neighbourhoods;
+    }
 
     @Override
     public void map(String key, TreeData treeData, Context<Tuple<String, String>, Integer> context) {
-        context.emit(new Tuple<>(treeData.getNeighbourhood(),treeData.getStreetName()), ONE);
+        if (neighbourhoods.contains(treeData.getNeighbourhood())) {
+            context.emit(new Tuple<>(treeData.getNeighbourhood(), treeData.getStreetName()), ONE);
+        }
     }
 }
